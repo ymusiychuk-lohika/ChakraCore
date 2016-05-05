c7ae96fcf90ed96a31dc01de66292837e939cb14
Sandeep Agarwal <saagarwa@microsoft.com>
2016-05-05 15:34:25 -0700

    Add copyright notice to new test files

--- test/Debugger/JsrtDebugUtilsAddPropertyType.js
+++ test/Debugger/JsrtDebugUtilsAddPropertyType.js
@@ -1,3 +1,8 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
 var obj = {
   a1 : undefined,
   a2 : null,
